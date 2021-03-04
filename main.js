b622be34886d2a9c5b9daf6f2eda49b36c669c3e
Semir Kardovikj <semir.k1612@gmail.com>
2021-03-04 15:50:17 +0100

    Created cards for each day

--- main.js
+++ main.js
@@ -8,8 +8,7 @@ window.addEventListener('load', () => {
 let weatherData;
 
 // DOM elements
-
-const daysContainer = document.getElementById('days_container')
+const daysContainer = document.getElementById('days_list');
 
 
 // Fetch weather data func
@@ -17,16 +16,54 @@ const getWeatherData = async () => {
     const res = await fetch('./weatherData.json')
     const data = await res.json()
     weatherData = data
-    renderDays()
+    renderDays()  
 }
 
 
 // Render days func
-
 const renderDays = () => {
     weatherData.days.forEach(day => {
-        daysContainer.innerHTML += `<li>${day.day}</li>`
+        daysContainer.innerHTML += `<li class="day_card id="day_card">
+        <h1 class="day_name">${getFullDayName(day.day)}</h1>
+        <h2 class="weather_type">${getWeatherType(day.type)}</i></h2>
+        <p class="weather_temp">${day.temp} ${weatherData.tempUnit}</p>
+        </li>`;
     });
 }
 
 
+// Get weather type func
+const getWeatherType = (type) => {
+    if(type === "sunny"){
+        return `<i class="fas fa-sun fa-3x"></i>`;
+    }else if (type === "rainy") {
+        return `<i class="fas fa-cloud-showers-heavy fa-3x"></i>`
+    }else if(type === "cloudy"){
+        return `<i class="fas fa-cloud fa-3x"></i>`
+    }
+}
+// Get full day name 
+const getFullDayName = (day) => {
+    switch(day){
+        case "Mon" : 
+            return "Monday";
+            break;
+        case "Tue" : 
+            return "Tuesday";
+            break;
+        case "Wed" : 
+            return "Wednesday"
+            break;
+        case "Thur" :
+            return "Thursday";
+            break;
+        case "Fri" : 
+            return "Friday"
+            break;
+        case "Sat" : 
+            return "Saturday"
+            break;
+        case "Sun" : 
+            return "Sunday"
+    }
+}
