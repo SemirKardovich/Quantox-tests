a4ab6a4071b992df2a7e6e78dea9e5c2aab27ea6
Semir Kardovikj <semir.k1612@gmail.com>
2021-03-04 13:41:59 +0100

    Fetched data successfully

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-alert('Hi')
+// On load get weather data
+window.addEventListener('load', () => {
+    getWeatherData();
+  });
+
+
+// Weather data
+let weatherData;
+
+// DOM elements
+
+const daysContainer = document.getElementById('days_container')
+
+
+// Fetch weather data func
+const getWeatherData = async () => {
+    const res = await fetch('./weatherData.json')
+    const data = await res.json()
+    weatherData = data
+    renderDays()
+}
+
+
+// Render days func
+
+const renderDays = () => {
+    weatherData.days.forEach(day => {
+        daysContainer.innerHTML += `<li>${day.day}</li>`
+    });
+}
+
+
